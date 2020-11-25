59bd93b58cc02e77d5663319246387840c5d79cd
Thomas More <tmore1@gmx.com>
2020-11-25 11:07:58 -0500

    Version 0.2
     * Added importing of Signal backups (decoded via signal-backup-decode)
    
     * Expanded statistics display
    
     * Added printing of program version
    
     * Expanded and corrected documentation

--- querying.md
+++ querying.md
@@ -1,35 +1,35 @@
-Here are some examples of querying the sms-db database. Much of the information here will be elementary to someone proficient in SQLite; I am an SQLite newbie, however, and these examples may be useful to others like me.
+Here are some examples of querying the sms-db database. Much of the information here will be elementary to someone proficient in [SQLite](https://www.sqlite.org/index.html); I am an SQLite novice, however, and these examples may be useful to others like me.
 
-Install the SQLite command line interface (in Debian, it's in the sqlite3 package), and run it in interactive mode, e.g.:
+Install the SQLite command line interface (in Debian, it's in the `sqlite3` package), and run it in interactive mode, e.g.:
 
 	sqlite3 -column -header sms.db
 	
 List all messages sent by Alice:
 
-	SELECT sender_name,sender_address,recipient_name,recipient_address,timestamp WHERE sender_name = 'Alice' ORDER BY timestamp;
+	SELECT sender_name,sender_address,recipient_name,recipient_address,timestamp FROM messages WHERE sender_name = 'Alice' ORDER BY timestamp;
 	
 To match all sender names that begin with 'Alice', use the `LIKE` operator:
 
-	SELECT sender_name,sender_address,recipient_name,recipient_address,timestamp WHERE sender_name LIKE 'Alice%' ORDER BY timestamp;
+	SELECT sender_name,sender_address,recipient_name,recipient_address,timestamp FROM messages WHERE sender_name LIKE 'Alice%' ORDER BY timestamp;
 	
 Or for all sender names that include 'Alice' within them:
 
-	SELECT sender_name,sender_address,recipient_name,recipient_address,timestamp WHERE sender_name = '%Alice%' ORDER BY timestamp;
+	SELECT sender_name,sender_address,recipient_name,recipient_address,timestamp FROM messages WHERE sender_name = '%Alice%' ORDER BY timestamp;
 	
 All messages sent or received on July 4, 2020 (GMT-04:00)
 
-	SELECT sender_name,sender_address,recipient_name,recipient_address,timestamp WHERE timestamp BETWEEN 1593835200000 AND 1593921600000 ORDER BY timestamp;
+	SELECT sender_name,sender_address,recipient_name,recipient_address,timestamp FROM messages WHERE timestamp BETWEEN 1593835200000 AND 1593921600000 ORDER BY timestamp;
 
 (Currently, all timestamps are in [Epoch time format](https://en.wikipedia.org/wiki/Unix_time), in milliseconds. To convert to or from a human readable date, use `date -d'@nnnnnnnnnn.nnn'` / `date -d'human_readable_date' +%s` (the decimal point must be inserted in the input, and '000' must be added to the output, since the `date` command date string format is in seconds rather than milliseconds), or use [this online converter](https://www.epochconverter.com/).
 
 All messages sent by Alice prior to July 4, 2020:
 
-	select sender_name,recipient_name,timestamp from messages where timestamp < 1593835200000 AND sender_name = 'Alice' ORDER BY timestamp;
+	select sender_name,recipient_name,timestamp FROM messages where timestamp < 1593835200000 AND sender_name = 'Alice' ORDER BY timestamp;
 	
 All the previous commands display only message metadata, not data. To display the data as well, add an `INNER JOIN` clause:
 
-	select sender_name,recipient_name,timestamp,data from messages INNER JOIN parts ON messages._id = parts.message_id where sender_name = 'Alice' ORDER BY timestamp;
+	select sender_name,recipient_name,timestamp,data FROM messages INNER JOIN parts ON messages._id = parts.message_id where sender_name = 'Alice' ORDER BY timestamp;
 
 To include only text parts, check the `content_type` column:
 
-	select sender_name,recipient_name,timestamp,data from messages INNER JOIN parts ON messages._id = parts.message_id where sender_name = 'Alice' and content_type LIKE 'text%' ORDER BY timestamp;
+	select sender_name,recipient_name,timestamp,data FROM messages INNER JOIN parts ON messages._id = parts.message_id where sender_name = 'Alice' and content_type LIKE 'text%' ORDER BY timestamp;
--- sms-db.pl
+++ sms-db.pl
@@ -10,22 +10,16 @@
 # sms-db is documented in its README.md
 
 use Getopt::Std;
-
 use XML::LibXML '1.70';
-#use Text::CSV;
 use DBI;
-use DBD::SQLite::Constants qw/:extended_result_codes/;
-
+use DBD::SQLite::Constants qw(:extended_result_codes);
 use Try::Tiny;
 use Time::HiRes qw(gettimeofday tv_interval);
 use Encode;
 use Digest::SHA qw(sha256_hex);
-
 use Data::Dump;
 
 use strict;
-
-my ($program_version, $database_version) = (0.1, 1.0);
 
 # configuration
 
@@ -37,21 +31,25 @@ unless (defined $opts{'f'}) {die "A format must be specified via '-f format'\n"}
 
 # constant definitions
 
-my ($XML, $BUGLE) = (0, 1);
+my ($XML, $BUGLE, $SIGNAL, $PROGRAM_VERSION, $DATABASE_VERSION) = (0, 1, 2, "0.2", 1.0);
 my @message_fields = ('timestamp', 'sender_address', 'sender_name', 'recipient_address', 'recipient_name', 'msg_box', 'source_format');
+
+# start
+
+print "sms-db version $PROGRAM_VERSION\n";
 
 # open / create database
 
 my $dbh = DBI->connect("dbi:SQLite:$database", undef, undef, {RaiseError => 1, PrintError => 0, AutoCommit => 0, sqlite_extended_result_codes => 1});
 unless ($dbh->tables(undef, undef, 'messages', 'TABLE')) {
 	$dbh->do("CREATE TABLE messages(_id INTEGER PRIMARY KEY AUTOINCREMENT,timestamp INT,sender_address TEXT,sender_name TEXT,recipient_address TEXT,recipient_name TEXT,msg_box INT,source_format INT,hash INT UNIQUE)");
+	$dbh->do("PRAGMA user_version = $DATABASE_VERSION");
 }
 unless ($dbh->tables(undef, undef, 'parts', 'TABLE')) {
 	$dbh->do("CREATE TABLE parts(_id INTEGER PRIMARY KEY AUTOINCREMENT,message_id INT,data TEXT,content_type TEXT,FOREIGN KEY (message_id) REFERENCES messages(_id) ON DELETE CASCADE)");
 	# this is a simplified version of the bugle 'parts' table, whose CREATE statement is:
 	# CREATE TABLE parts(_id INTEGER PRIMARY KEY AUTOINCREMENT,message_id INT,text TEXT,uri TEXT,content_type TEXT,width INT DEFAULT(-1),height INT DEFAULT(-1),timestamp INT, conversation_id INT NOT NULL,FOREIGN KEY (message_id) REFERENCES messages(_id) ON DELETE CASCADE FOREIGN KEY (conversation_id) REFERENCES conversations(_id) ON DELETE CASCADE )
 }
-$dbh->do("PRAGMA user_version = $database_version");
 my $message_sth = $dbh->prepare("INSERT INTO messages(" . join(',', (@message_fields, 'hash')) . ") VALUES(" . join(',', (('?') x ($#message_fields + 2))) . ")");
 my $part_sth = $dbh->prepare("INSERT INTO parts(message_id,data,content_type) VALUES(?,?,?)");
 my ($total_messages, $inserted_messages, $duplicate_messages, $ignored_messages, $total_parts, $start_time) = (0, 0, 0, 0, 0, [gettimeofday]);
@@ -85,7 +83,6 @@ if (defined $opts{'i'}) {
 			insert(\%message, \@parts);
 		}
 	}
-	
 	elsif ($opts{'f'} eq 'bugle') {
 		my $bugle = DBI->connect("dbi:SQLite:$opts{'i'}", undef, undef, {RaiseError => 1, PrintError => 0, AutoCommit => 0, sqlite_extended_result_codes => 1});
 		my @messages = $bugle->selectall_array("SELECT messages._id,received_timestamp,sender_info.normalized_destination,sender_info.full_name,participant_normalized_destination,participant_count,name,sub_id FROM messages INNER JOIN participants sender_info ON messages.sender_id = sender_info._id INNER JOIN conversations ON messages.conversation_id = conversations._id", {Slice => {}});
@@ -101,9 +98,101 @@ if (defined $opts{'i'}) {
 		}
 		$bugle->disconnect;
 	}
+	elsif ($opts{'f'} eq 'signal') {
+		my $signal = DBI->connect("dbi:SQLite:$opts{'i'}/signal_backup.db", undef, undef, {RaiseError => 1, PrintError => 0, AutoCommit => 0, sqlite_extended_result_codes => 1});
+		my %message_types = (23 => 2, 87 => 2, 88 => 2, 10485783 => 2, 20 => 1, 10485780 => 1);
+		# I'm not really sure what the message types all mean. I'm assuming, based on the contents of my Signal backup database, that the ones I've assigned to '2' are roughly equivalent to 'sent' and the ones I've assigned to '1' are roughly equivalent to 'received'. We use the Syntech XML 'type' / 'msg_box' field values (https://synctech.com.au/sms-backup-restore/fields-in-xml-backup-files/) internally to represent 'sent' and 'received'
+		
+		# process sms messages
+		
+		my @smss = $signal->selectall_array("SELECT address,date,type,body,phone,system_display_name FROM sms INNER JOIN recipient ON sms.address = recipient._id", {Slice => {}});
+		foreach (@smss) {
+			my %message = (source_format => $SIGNAL, timestamp => $_->{date});
+			unless (defined $message_types{$_->{type}}) {
+				warn "Unknown message type '$_->{type}' - ignoring message.\n";
+				dd $_;
+				$total_messages++;
+				$ignored_messages++;
+				next;
+			}
+			($message{'msg_box'}, $message{'sender_address'}, $message{'sender_name'}, $message{'recipient_address'}, $message{'recipient_name'}) =
+				($message_types{$_->{type}} eq 2) ? (2, "<SELF>", "<SELF>", $_->{phone}, $_->{system_display_name} // "<UNAVAILABLE>") : (1, $_->{phone}, $_->{system_display_name} // "<UNAVAILABLE>", "<SELF>", "<SELF>");
+			my @parts;
+			push @parts, {data => $_->{body}, content_type => 'text/plain'};
+			insert(\%message, \@parts);
+		}
+		
+		# process mms messages
+		my $group_sth = $signal->prepare("SELECT members FROM groups WHERE group_id = ?");
+		my $member_sth = $signal->prepare("SELECT phone,system_display_name FROM recipient WHERE _id = ?");
+		my $thread_sth = $signal->prepare("SELECT recipient_ids FROM thread WHERE _id = ?");
+		my $recipient_sth = $signal->prepare("SELECT group_id FROM recipient WHERE _id = ?");
+		unless (opendir(DIR, "$opts{'i'}/attachment")) {warn "Can't open '$opts{'i'}/attachment': $!"; next}
+		my @attachment_filenames;
+		unless (@attachment_filenames = readdir(DIR)) {warn "Can't read directory '$opts{'i'}/attachment': $!"; next}
+		closedir (DIR);
+		my @attachment_parts = $signal->selectall_array("SELECT mid,ct,unique_id FROM part", {Slice => {}});
+		my @mmss = $signal->selectall_array("SELECT mms._id,thread_id,address,date,msg_box,body,phone,system_display_name,group_id FROM mms INNER JOIN recipient ON mms.address = recipient._id", {Slice => {}});
+		foreach (@mmss) {
+			my %message = (source_format => $SIGNAL, timestamp => $_->{date});
+			unless (defined $message_types{$_->{msg_box}}) {
+				warn "Unknown message type '$_->{msg_box}' - ignoring message.\n";
+				dd $_;
+				$total_messages++;
+				$ignored_messages++;
+				next;
+			}
+			if (defined $_->{group_id}) {
+				my (@phones, @system_display_names);
+				my $members = $signal->selectrow_array($group_sth, {}, $_->{group_id});
+				foreach (split(/,/, $members)) {
+					($phones[$#phones +1], $system_display_names[$#system_display_names + 1]) = $signal->selectrow_array($member_sth, {}, $_);
+				}
+				$_->{phone} = join(',', @phones);
+				$_->{system_display_name} = join(',', @system_display_names);
+				#dd @phones, @system_display_names;
+			}
+			($message{'msg_box'}, $message{'sender_address'}, $message{'sender_name'}, $message{'recipient_address'}, $message{'recipient_name'}) =
+				($message_types{$_->{msg_box}} eq 2) ? (2, "<SELF>", "<SELF>", $_->{phone}, $_->{system_display_name} // "<UNAVAILABLE>") : (1, $_->{phone}, $_->{system_display_name} // "<UNAVAILABLE>", "<SELF>", "<SELF>");
+			my $recipient_ids = $signal->selectrow_array($thread_sth, {}, $_->{thread_id});
+			my $group_id = $signal->selectrow_array($recipient_sth, {}, $recipient_ids);
+			my ($recipient_phones, $recipient_system_display_names);
+			if (defined $group_id) {
+				my (@phones, @system_display_names);
+				my $members = $signal->selectrow_array($group_sth, {}, $group_id);
+				#print $group_id, "\n"; dd $members;
+				foreach (split(/,/, $members)) {
+					($phones[$#phones +1], $system_display_names[$#system_display_names + 1]) = $signal->selectrow_array($member_sth, {}, $_);
+				}
+				$recipient_phones = join(',', @phones);
+				$recipient_system_display_names = join(',', @system_display_names);
+				#dd @phones, @system_display_names;
+			}
+			if (defined $recipient_phones) {$message{'recipient_address'} = $recipient_phones};
+			if (defined $recipient_system_display_names) {$message{'recipient_name'} = $recipient_system_display_names};
+			my $mid = $_->{_id};
+			my @parts;
+			if (defined $_->{body}) {push @parts, {data => $_->{body}, content_type => 'text/plain'}}
+			foreach (@attachment_parts) {
+				next unless ($_->{mid} eq $mid);
+				my $unique_id = $_->{unique_id};	
+				my $filename;
+				foreach (@attachment_filenames) {if (/^${unique_id}_.*$/) {$filename = $_; last}}
+				unless (defined $filename) {warn "File not found for attachment with unique_id '$unique_id'\n"; next}
+				unless (open (ATTACHMENT, '<', "$opts{'i'}/attachment/$filename")) {warn "Can't open '$filename': $!"; next}
+				my $attachment = do {local $/; <ATTACHMENT>};
+				close ATTACHMENT;
+				push @parts, {data => $attachment, content_type => $_->{ct}};
+			}
+			#unless (@parts) {warn "Message has no parts - skipping.\n"; dd $_; $total_messages++; $ignored_messages++; next}
+			insert(\%message, \@parts);
+		}
+		$signal->disconnect;
+	}
 	else {die "Unknown format '$opts{'f'}'\n"}
 	$dbh->commit;
-	print "Ignored messages:\t$ignored_messages\nTotal messages:\t\t$total_messages\nInserted messages:\t$inserted_messages\nDuplicate messages:\t$duplicate_messages\nTotal parts:\t\t$total_parts\nElapsed time:\t\t", tv_interval($start_time), " seconds\n\n";
+	my $rows = $dbh->selectrow_array("SELECT COUNT(1) FROM messages");
+	print "Total messages seen:\t\t$total_messages\nTotal messages imported:\t$inserted_messages\nDuplicate messages:\t\t$duplicate_messages\nIgnored messages:\t\t$ignored_messages\nTotal message parts imported:\t$total_parts\nMessages in database:\t\t$rows\nElapsed time:\t\t\t", tv_interval($start_time), " seconds\n\n";
 }
 elsif (defined $opts{'o'}) {
 	die "Output ('-o') is not yet implemented.\n";
